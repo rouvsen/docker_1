9a9860efb8cf4f3e7bb13512ac1bed72413147a0
rovshan <rouvsen@gmail.com>
2023-03-30 00:17:00 +0400

    codes added

---
+++ 1-node-app-ubuntu/index.js
@@ -0,0 +1,3 @@
+console.log("Hello from Ubuntu");
+console.log("ENV => ", process.env.channel);
+console.log("Node app 1")
---
+++ 4-php-app/index.php
@@ -0,0 +1,3 @@
+<?php
+    echo "PHP Application Dockerize Etmek ... | rouvsen";
+?>
